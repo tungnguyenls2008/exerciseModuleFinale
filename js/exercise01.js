87ba553ebc107fe2c6dff604f2af2712a94220a5
tung.nguyen <tungnguyenls2008@github.com>
2020-03-30 16:41:00 +0700

    exercise module 01 finale

--- js/exercise01.js
+++ js/exercise01.js
@@ -1,15 +1,10 @@
 function maxArray(arr) {
 
     let max = arr[0];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
         }
-    document.getElementById("display3").innerHTML= "highest grade is: "+max;
     }
-/*let arr = [2, 9, 3, 10, 0, 4, 8, 6, 9];
-let max = maxArray(arr);*/
-
-/*
-maxArray(arr);*/
+    document.getElementById("display3").innerHTML = "highest grade is: " + max;
+}
--- js/exercise03.js
+++ js/exercise03.js
@@ -1,12 +1,12 @@
-
 function isFibonacci(num) {
-console.log(num);
-    let fibonacciSequence = [0,1];
-    for (let i=0; i < 500; i++) {
+    console.log(num);
+    let fibonacciSequence = [0, 1];
+    for (let i = 0; i < 500; i++) {
         fibonacciSequence.push(fibonacciSequence[i] + fibonacciSequence[i + 1]);
-        if(num===fibonacciSequence[i]){
-            document.getElementById('display').innerHTML=num+" is a Fibonacci number.";break;
-        }else document.getElementById('display').innerHTML=num+" is NOT a Fibonacci number.";
+        if (num === fibonacciSequence[i]) {
+            document.getElementById('display').innerHTML = num + " is a Fibonacci number.";
+            break;
+        } else document.getElementById('display').innerHTML = num + " is NOT a Fibonacci number.";
 
     }
     console.log(fibonacciSequence);
